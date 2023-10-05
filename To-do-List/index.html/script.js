b7287cb20650a5626695fdbf2f972a5ec1716670
Ivonia Fatima Viegas <127854815+Bibba22@users.noreply.github.com>
2023-10-05 05:32:12 -0700

    Add files via upload

---
+++ To-do-List/index.html/script.js
@@ -0,0 +1,36 @@
+const inputBox = document.gentElementById("input-box");
+const inputBox = document.gentElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();    
+}
+
+list-container.addEventListener("click", function(e){
+    if(e.target.tagName === "li"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
